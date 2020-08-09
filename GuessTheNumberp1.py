014a6de722b11eb29d89157a4f6cac4829fb4fa6
Swaraj Birje <64256098+SwarajEPICoder@users.noreply.github.com>
2020-08-09 20:03:02 +0530

    Update GuessTheNumberp1.py
    Added comments to the file for easier understanding of code.

--- GuessTheNumberp1.py
+++ GuessTheNumberp1.py
@@ -1,9 +1,21 @@
+#Importing random to use the functions
 from random import randint
+
+#Setting the random number(integer)
 num = randint(1,20)
+
+### MAIN CODE ###
+
+#In this 6-iteration loop all the main code is executed
 for guesses in range(1,6):
+    
+    #Asking for input and converting it into an integer
     guess = input('type in a number: ')
     guess = int(guess)
-
+    
+    ### MAIN LOGIC ###
+    # Checking if the guess is equal, higher or lower than the numver to guess respectively
+    
     if guess == num:
         print('Yay! you got it right')
         break
